1e77b789b0c3135bb55154b2da254066958c96a5
Lotosney <sebastiandwojcik@gmail.com>
2021-05-13 09:57:38 +0200

    Add puck

--- script.js
+++ script.js
@@ -23,6 +23,10 @@ class Player {
     constructor() {
         this.x = undefined
         this.y = undefined
+        this.prevX = undefined
+        this.prevY = undefined
+        this.dx = undefined
+        this.dy = undefined
  
     }
 
@@ -33,13 +37,53 @@ class Player {
         context.fill()
         context.stroke()
     }
+    update() {
+        this.dx = this.x - this.prevX
+        this.dy = this.y - this.prevY
+        this.prevX = this.x
+        this.prevY = this.y
+    }
 
 }
+
+class Puck {
+
+    constructor() {
+        this.x = width/2
+        this.y = height/2
+        this.dx = 1
+        this.dy = 1
+    }
+
+    draw() {
+        context.beginPath()
+        context.arc(this.x, this.y , width*.04 ,0 ,2*Math.PI)
+        context.fillStyle = "black"
+        context.fill()
+        context.stroke()
+    }
+    update(){
+        this.x +=this.dx
+        this.y += this.dy
+        if(this.x + width*.04 > width || this.x - width*.04 < 0) {
+            this.dx *= -1
+        }
+
+        if(this.y + width*.04 > height || this.y - width*.04 < 0) {
+            this.dy *= -1
+        }
+    }
+}
 const player = new Player
+const puck = new Puck
 
 function animate() {
     context.clearRect(0,0,width,height)
     player.draw()
+    player.update()
+
+    puck.draw()
+    puck.update()
     board()    
     requestAnimationFrame(animate)
 }
